d1344efc07e3e7b2fb9dbe76f43b06e0e13b14dd
Rasyadan F <129726668+rsydnf@users.noreply.github.com>
2024-07-12 01:57:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import fibonacci
+from fibonacci import fibonacci
+
+def main():
+    numTerms = int(input("Enter the number of Fibonacci terms: "))
+    numTerms <= 0
+
+seq = fibonnaci(numTerms)
+print ("The Fibonacci sequence is:".join(map(str, sequence)))
